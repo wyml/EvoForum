391613f43cd44114a924ad9b764ba7dac33936b5
Kingsr <1143524493@qq.com>
2022-08-23 15:30:41 +0800

    初始化路由

---
+++ bootstrap/route.go
@@ -0,0 +1,41 @@
+package bootstrap
+
+import (
+	"forum/routes"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(router *gin.Engine) {
+	registerGlobalMiddleware(router)
+
+	routes.RegisterAPIRouters(router)
+
+	setup404Handler(router)
+}
+
+func registerGlobalMiddleware(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(router *gin.Engine) {
+	router.NoRoute(func(c *gin.Context) {
+		// 获取标头的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+}
