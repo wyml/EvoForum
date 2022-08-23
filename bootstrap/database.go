1db5ebab34b703a376217e14bceee69d239a108d
Kingsr <1143524493@qq.com>
2022-08-24 01:16:35 +0800

    数据库请求日志

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"forum/pkg/config"
 	"forum/pkg/database"
+	"forum/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"time"
 )
 
@@ -38,7 +38,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
